4bf5a5529239b3ed6f7c3bb757a766a41d11a0b2
Егор Ивков <e.o.ivkov@gmail.com>
2018-04-08 00:09:12 +0300

    Something working

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,20 +7,28 @@ using UnityEngine.Networking.NetworkSystem;
 public class GameManager : NetworkManager {
 
     public int maxPlayers = 2;
-    private List<GameObject> players = new List<GameObject>();
     public GameObject server;
+    public GameObject playerBluePrefab;
+    public GameObject playerRedPrefab;
 
     public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
     {
         server = GameObject.Find("GameServer");
-        var player = (GameObject)GameObject.Instantiate(playerPrefab, new Vector3(0,0,0), Quaternion.identity);
-        player.GetComponent<PlayerController>().ID = playerControllerId;
-        NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
-        server.GetComponent<ServerBehaviour>().players.Add(player);
-        Debug.Log(playerControllerId);
-        if(players.Count > 1)
+            
+        if(server.GetComponent<ServerBehaviour>().players.Count > 0)
         {
+            var player = (GameObject)GameObject.Instantiate(playerRedPrefab, new Vector3(-3.05f, -0.68f, 0), Quaternion.identity);
+            player.GetComponent<PlayerController>().ID = playerControllerId;
+            server.GetComponent<ServerBehaviour>().players.Add(player);
+            Debug.Log(playerControllerId);
+            NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
             server.GetComponent<ServerBehaviour>().state = ServerBehaviour.State.Start;
+        } else {
+            var player = (GameObject)GameObject.Instantiate(playerBluePrefab, new Vector3(3.66f, -0.68f, 0), Quaternion.Euler(0, -180, 0));
+            player.GetComponent<PlayerController>().ID = playerControllerId;
+            server.GetComponent<ServerBehaviour>().players.Add(player);
+            Debug.Log(playerControllerId);
+            NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
         }
     }
 
