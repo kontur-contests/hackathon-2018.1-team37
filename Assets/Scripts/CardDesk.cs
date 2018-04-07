ad04ccaa310db18c5c30a1725b1c72f2509d8b95
Anton Skudarnov <anybis-maat@mail.ru>
2018-04-07 17:27:13 +0300

    CardDesk Init Commit

---
+++ Assets/Scripts/CardDesk.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class CardDesk : ScriptableObject {
+
+
+
+    public List<Card> cardDesk;
+	
+    
+    // Use this for initialization
+	void Start () {
+        var cards = Resources.LoadAll("Cards", typeof(Card)).Cast<Card>().ToArray();
+
+        for(int i = 0; i < cards.Length; i++)
+        {
+            cardDesk.Add(cards[i]);
+        }
+	}
+
+
+	
+	
+}
