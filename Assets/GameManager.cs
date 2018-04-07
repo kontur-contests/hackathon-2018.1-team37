bd2820c2924e79005569bc58391396438f265f38
Егор Ивков <e.o.ivkov@gmail.com>
2018-04-07 14:57:51 +0300

    Server Behaviour - untestes

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -9,13 +9,15 @@ public class GameManager : NetworkManager {
     public int maxPlayers = 2;
     private int playerCount = 0;
     private List<GameObject> players = new List<GameObject>();
+    public GameObject server;
 
     public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
     {
+        server = GameObject.Find("GameServer");
         var player = (GameObject)GameObject.Instantiate(playerPrefab, new Vector3(0,0,0), Quaternion.identity);
         player.GetComponent<PlayerController>().id = playerControllerId;
         NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
-        players.Add(player);
+        server.GetComponent<ServerBahaviour>().players.Add(player);
         Debug.Log(playerControllerId);
     }
 
---
+++ Assets/ServerBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ServerBahaviour : NetworkBehaviour
+{
+
+
+    [SyncVar]
+    public List<GameObject> players = new List<GameObject>();
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
