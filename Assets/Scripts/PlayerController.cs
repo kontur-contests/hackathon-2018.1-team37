9c1b0c2bd1078101c3739a6073b58de8d90756d9
Anton Skudarnov <anybis-maat@mail.ru>
2018-04-07 15:43:43 +0300

    Player Input Configured

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,10 +9,47 @@ public class PlayerController : MonoBehaviour {
 	void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetKeyDown(KeyCode.A))
-            Debug.Log("A PRESSED");
-	}
+
+
+    private bool m_ShotAxis = false;
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetButtonDown("Socket1")){
+            Debug.Log("A");
+        }
+        if (Input.GetButtonDown("Socket2"))
+        {
+            Debug.Log("X");
+        }
+        if (Input.GetButtonDown("Socket3") )
+        {
+            Debug.Log("Y");
+        }
+        
+        if(Input.GetButtonDown("Socket4"))
+        {
+            Debug.Log("B");
+        }
+
+      
+
+
+
+
+        //Gamepad Shot
+        if (Input.GetAxisRaw("Shot") != 0)
+        {
+            if (m_ShotAxis == false)
+            {
+                Debug.Log("RT");
+                // Call your event function here.
+                m_ShotAxis = true;
+            }
+        }
+        if (Input.GetAxisRaw("Shot") == 0)
+        {
+            m_ShotAxis = false;
+        }
+    }
 }
