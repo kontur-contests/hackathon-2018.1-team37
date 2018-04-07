783962a2c2bcbf87ee34cc8356954cbc7c9e3467
Anton Skudarnov <anybis-maat@mail.ru>
2018-04-07 14:22:16 +0300

    Few Damage Types in action added. DropChance and Texture fields added.

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -2,17 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public enum Action
 {
-	Damage,
+	DamageEnemy,
+	DamageSelf,
+	DamageBoth,
 	Heal,
 	Evade
 }
 
-
+[CreateAssetMenu(fileName = "Card", menuName = "WildCard/Card", order = 1)]
 public class Card : ScriptableObject {
 
-	public Action type;
-	public int value;
+	public Action _ActionType;
+	public int _value;
+	public float _dropChance;
+	public Texture Texture;
 
 }
